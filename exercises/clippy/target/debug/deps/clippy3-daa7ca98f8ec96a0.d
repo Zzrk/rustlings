93c4dfdfbd58a7b883a1f968707474e607aa874d
Zzrk <1252124617@qq.com>
2023-08-29 00:00:51 +0800

    feat: upload exercises

---
+++ exercises/clippy/target/debug/deps/clippy3-daa7ca98f8ec96a0.d
@@ -0,0 +1,8 @@
+F:\Rust\rustlings\exercises\clippy\target\debug\deps\clippy3-daa7ca98f8ec96a0.rmeta: clippy3.rs Cargo.toml
+
+F:\Rust\rustlings\exercises\clippy\target\debug\deps\clippy3-daa7ca98f8ec96a0.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
